7d7f9a547922b2e4abbc4bf5cb89d1bcb923f591
Paul Nix <kenpopaul@yahoo.co.uk>
2023-07-15 20:20:02 +0100

    icons

--- social-app/src/components/Sidebar.tsx
+++ social-app/src/components/Sidebar.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faHashtag,
+  faBell,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar: React.FC = () => {
-  return <div className="sidebar">{/* Sidebar content */}</div>;
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={faHome} />
+          <span>Home</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faHashtag} />
+          <span>Explore</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faBell} />
+          <span>Notifications</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span>Messages</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
